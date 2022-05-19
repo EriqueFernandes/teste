55fe62615b58a449d8cf03fd4ea3283ffc562517
Erique Fernandes <99293450+EriqueFernandes@users.noreply.github.com>
2022-05-19 09:24:32 -0300

    update

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang "en">
+    <head>
+        <meta charset = "UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" contet = "width = device-width, initial-scale = 1.0">
+        <title>Spes</title>
+        <link rel = "shortcut icon" href = "iconHead/logoHead.webp">
+        <link rel="stylesheet" href="css/styleUL.css">
+
+</head>
+<body>
+    <div class="container-all">
+        <nav>
+            <ul>
+                <a href = "index.php" ><img class = "imgUL" src="iconUL/logoFocus.png"></a>
+                <!-- <li>INÍCIO</li> -->
+                <li>CRONOGRAMA</li>
+                <li>MOTIVADORES</li>
+                <li>CENTRO DE APOIO</li>
+            </ul>
+            <ul>
+                <li>Login</li>
+                <li>Sobre</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="container">
+        <header>
+            <h1 class = "pagPrincipal" >Página Principal</h1>
+        </header>
+        <div class = "partitions">
+            <section>
+                <h3 class = "hPrincipal2" >O melhor para o seu estudo!</h3>
+                <img class = "imgPrincipal" src = "iconContainer/crono.png">
+            </section>
+            <article>
+                <section>
+                    <h3>Semana 01</h3>
+                    <img src = "https://jcconcursos.com.br/media/_versions/noticia/concurso-trf4-prova-e-gabarito_widelg.jpg">
+                </section>
+                <section>
+                    <h3>Semana 02</h3>
+                    <img src = "http://uab.ufsc.br/administracao/files/2020/06/provas.jpg">
+                </section>
+                <!-- <section>
+                    <h3>Semana 02</h3>
+                    <img src = "http://uab.ufsc.br/administracao/files/2020/06/provas.jpg">
+                </section> -->
+            </article>
+    </div>
+</body>
+</html>
